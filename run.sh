afb21fd2b02a2eb1ad5f95e7634465605b6cfcdd
Marcel Wysocki <maci.stgn@gmail.com>
2015-01-13 16:35:16 +0100

    fix

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo date
-CONTAINER=$(sudo docker run --name ipa.example.com -d -h ipa.replynet.prv -v /sys/fs/cgroup:/sys/fs/cgroup:ro maci/freeipa-server /usr/lib/systemd/systemd)
+CONTAINER=$(sudo docker run --name ipa.example.com -d -h ipa.example.com -v /sys/fs/cgroup:/sys/fs/cgroup:ro maci/freeipa-server /usr/lib/systemd/systemd)
 echo ${CONTAINER}
 echo "To enter docker container run: 
         sudo docker exec -t -i ${CONTAINER} /bin/bash"
